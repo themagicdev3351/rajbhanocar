cf048ec7664b1ec7d8e8ac85d514ceec82e0e1ab
themagicdev3351 <themagicdev3351@gmail.com>
2024-03-22 15:15:02 +0530

    er

--- backend/backend/server.js
+++ backend/backend/server.js
@@ -10,14 +10,11 @@ const bodyParser = require('body-parser');
 connectDB();
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 
 app.use(express.json({ limit: '10mb' }));
-app.use(bodyParser.json({ limit: '10mb' }));
-
-
+app.use(bodyParser.json({ limit: '10mb' })); 
 
 app.get("/", (req, res) => {
   res.json({ message: "API running..." });
